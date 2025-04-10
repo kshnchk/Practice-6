b2ac6b2ae1b1be0b6dc38e8b7cfe1ae25a956c1b
kshnchk <kruglovaksusa31@gmail.com>
2025-04-10 21:26:14 +0400

    Add files via upload

---
+++ задание 6/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+В массиве хранятся цены на 15 видов товаров. С помощью датчика
+случайных чисел заполнить массив целыми значениями, лежащими в
+диапазоне от 20 до 300 включительно. Определить цену самого дешевого
+товара и его порядковый номер.
+*/
+
+namespace задание_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] prices = new int[15];
+            Random rand = new Random();
+            for (int i = 0; i < prices.Length; i++)
+            {
+                prices[i] = rand.Next(20, 301); 
+            }
+            Console.WriteLine("Цены на товары:");
+            for (int i = 0; i < prices.Length; i++)
+            {
+                Console.WriteLine($"Товар {i + 1}: {prices[i]}");
+            }
+            int minPrice = prices[0];
+            int minIndex = 0;
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] < minPrice)
+                {
+                    minPrice = prices[i];
+                    minIndex = i;
+                }
+            }
+            Console.WriteLine($"Самая дешевая цена: {minPrice} на товар {minIndex + 1}");
+        }
+    }
+}
---
+++ задание 7/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Задан одномерный массив чисел длины n (n – вводится с клавиатуры).
+Заполнить его числами Фибоначчи.
+Последовательность чисел Фибоначчи: n0 = 0, n1=1, …, ni = ni-1 + ni-2, где i –
+номер элемента последовательности; пример: n0=0, n1=1, n2=1+0=1,
+n3=1+1=2 и т.д. Если N=45, то последовательность будет: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
+*/
+
+namespace задание_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размер массива n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            long[] fibonacci = new long[n];
+            if (n >= 1)
+            {
+                fibonacci[0] = 0;
+            }
+            if (n >= 2)
+            {
+                fibonacci[1] = 1;
+            }
+            for (int i = 2; i < n; i++)
+            {
+                fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2]; 
+            }
+            Console.WriteLine("Последовательность Фибоначчи:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(fibonacci[i]);
+            }
+        }
+    }
+}
